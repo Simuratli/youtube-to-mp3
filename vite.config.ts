dce927a5567a59b41798948d08ef5758b26ab2cf
Simuratli Eljan <simuratli@gmail.com>
2024-02-16 17:16:50 +0400

    name

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
       manifest: {
         name: "Unkai converter",
-        short_name: "Unkai converter",
+        short_name: "Unkai  converter",
         theme_color: "#ffffff",
         icons: [
           {
